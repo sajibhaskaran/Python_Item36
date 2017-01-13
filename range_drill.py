ad2097a4e852ae6e947be1ce5a5a8356720147e4
Saji Bhaskaran <sajibhaskaran@outlook.com>
2017-01-13 15:33:52 -0800

    range_drill added

---
+++ range_drill.py
@@ -0,0 +1,29 @@
+
+'''
+DRILLS:
+1. Start IDLE and use the Python range() function with one parameter to display the
+following:
+0
+1
+2
+3
+'''
+
+for num in range(4):
+	print(num)
+	
+'''
+2. Use the Python range() function with 3 parameters to display the following:
+3 2 1 0
+'''
+
+for num in range(3, -1, -1):
+	print(num)
+
+'''
+3. Use the Python range() function with 3 parameters to display the following:
+8 6 4 2
+'''
+
+for num in range(8, 0, -2):
+	print(num)
